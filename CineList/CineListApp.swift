dc02dbe6f7d50251831ae4507458edf3cc3d3777
Breno <brenofreitas28f@gmail.com>
2026-08-18 09:31:54 -0300

    feat: initial commit

---
+++ CineList/CineListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineListApp.swift
+//  CineList
+//
+//  Created by breno.farias on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CineListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
